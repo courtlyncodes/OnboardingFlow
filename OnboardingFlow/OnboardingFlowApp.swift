37c8c157c3987bd5ec6fa9be7fc76d705ea83de5
Courtlyn Boykin <boykin.courtlyn@gmail.com>
2024-10-18 15:30:56 -0400

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Courtlyn on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
